26f75e30474996ed78eda869c0116fb0f1b33e13
cjwarrior7 <cj23021996@gmail.com>
2021-03-16 23:27:29 +0530

    Template looping in lwc

---
+++ force-app/main/default/lwc/loopingInLWC/loopingInLWC.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class LoopingInLWC extends LightningElement {
+       carList =['maruti','nexon','Scorpio','ertiga'];
+       ceoList =[{id:1,Company:'salesforce',name:'Marc Benioff'},
+       {id:2,Company:'tesla',name:'elon musk'},
+       {id:3,Company:'google',name:'sundar pichai'},
+       {id:4,Company:'microsoft',name:'satya nadella'}
+    ]
+}
